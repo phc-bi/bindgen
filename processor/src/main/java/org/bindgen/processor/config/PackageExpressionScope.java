2fb2ad6809552c39a2d6dc3da0b96514901a073c
David Shepherdson <david.shepherdson@precedencehealthcare.com>
2020-10-21 17:03:29 +1100

    Added basic support for using regular expressions to define the scope, which also (with the right regex) provides a way to exclude whole classes or sub-packages from processing.

--- processor/src/main/java/org/bindgen/processor/config/PackageExpressionScope.java
+++ processor/src/main/java/org/bindgen/processor/config/PackageExpressionScope.java
@@ -1,12 +1,14 @@
 package org.bindgen.processor.config;
 
-import joist.util.Join;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Pattern;
 
 import org.bindgen.processor.util.ClassName;
 
 /**
  * A {@link ClassName} scope that can filter packages based on a string expression.
- * 
+ *
  * The expression specifies all beginning parts of allowed packages
  * in a comma-separated string, eg {@code com.myapp.customers.domain,
  * com.myapp.users.domain} which will allow any ClassName that is in
@@ -17,14 +19,25 @@ import org.bindgen.processor.util.ClassName;
 // TODO unit test
 public class PackageExpressionScope implements Scope<ClassName> {
 
-	private final String[] expressions;
+	/**
+	 * If any input expression starts with this string,
+	 * it will be treated as a regular expression rather than a simple package name prefix.
+	 */
+	private static final String REGEX_MARKER = "regex:";
+	private final List<String> prefixes = new ArrayList<>();
+	private final List<Pattern> patterns = new ArrayList<>();
 
 	public PackageExpressionScope(String packageMask) {
-		this.expressions = packageMask.split(",");
-		for (int i = 0; i < this.expressions.length; i++) {
-			this.expressions[i] = this.expressions[i].trim();
-			if (!this.expressions[i].endsWith(".")) {
-				this.expressions[i] = this.expressions[i] + ".";
+		String[] expressions = packageMask.split(",");
+		for (String expression : expressions) {
+			expression = expression.trim();
+			if (expression.startsWith(REGEX_MARKER)) {
+				this.patterns.add(Pattern.compile(expression.substring(REGEX_MARKER.length())));
+			} else {
+				if (!expression.endsWith(".")) {
+					expression = expression + ".";
+				}
+				this.prefixes.add(expression);
 			}
 		}
 	}
@@ -33,8 +46,13 @@ public class PackageExpressionScope implements Scope<ClassName> {
 	@Override
 	public boolean includes(ClassName name) {
 		final String packageName = name.getPackageName() + ".";
-		for (String expression : this.expressions) {
-			if (packageName.startsWith(expression)) {
+		for (String prefix : this.prefixes) {
+			if (packageName.startsWith(prefix)) {
+				return true;
+			}
+		}
+		for (Pattern pattern : this.patterns) {
+			if (pattern.matcher(packageName).matches()) {
 				return true;
 			}
 		}
@@ -46,8 +64,10 @@ public class PackageExpressionScope implements Scope<ClassName> {
 	public String toString() {
 		return new StringBuilder("[")
 			.append(this.getClass().getSimpleName())
-			.append(" expression=")
-			.append(Join.join(this.expressions, ","))
+			.append(" prefixes=")
+			.append(String.join(",", this.prefixes))
+			.append(" patterns=")
+			//.append(String.join(",", this.patterns.stream().map(Pattern::pattern).collect(Collectors.toList())))
 			.append("]")
 			.toString();
 	}
